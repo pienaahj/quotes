8bb55c069db65e1974139d208614c74b9e3f9f83
Hendrik Pienaar <pienaahj@gmail.com>
2022-02-07 14:30:04 +0200

    added quotes function

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gitbub.com/pienaahj/quotes
+
+go 1.17
---
+++ quotes.go
@@ -0,0 +1,9 @@
+package quotes
+
+// favs return a slice of specific strings
+func favs() []string {
+	a := "first entry"
+	b := "second entry"
+	c:= []string {a,b}
+	return c
+}
